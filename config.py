f86057bb362be44ff55e04c4a305e2854696862d
GifeBiBede <lezgoboy1@gmail.com>
2026-07-16 05:18:25 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+
+DATABASE_NAME = "gifs.db"
+BOT_USERNAME = os.getenv("BOT_USERNAME", "")
